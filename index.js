597e5af5b8c33dfdf9a955763979ef92141cd8bb
Michael Moutu <moutumichael@gmail.com>
2026-04-16 20:56:31 +1000

    Added React App

--- index.js
+++ index.js
@@ -98,5 +98,5 @@ app.get(/.*/, (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
